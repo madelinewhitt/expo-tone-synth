7ac9d58453db2a219ff306254ca87b6586c79e98
madelinewhitt <madelinewhitton@MacBook-Air-10.local>
2024-08-28 12:27:50 -0400

    Refined needed functions

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,15 +1,10 @@
 export declare const getIsPlaying: () => any;
 export declare const playWhiteNoise: () => any;
-export declare const playPinkNoise: () => any;
-export declare const playBrownNoise: () => any;
-export declare const setWhiteNoiseAmplitude: (amplitude: number) => Promise<any>;
+export declare const adjustFrequency: (frequency: number) => any;
 export declare const playSoundFromURL: (URL: string) => any;
 export declare const stopSoundFromURL: (URL: string) => any;
 export declare const setVolumeForURL: (URL: string, amplitude: number) => any;
-export declare const pinkNoiseTest: (coefficient1: number, coefficient2: number, coefficient3: number, coefficient4: number, coefficient5: number, coefficient6: number, pitch: number) => any;
-export declare const changePitch: (pitch: number) => any;
-export declare const play: (frequency: number, amplitudes: number[], adsr: number[]) => Promise<any>;
 export declare const performFFT: () => any;
-export declare const setFrequency: (frequency: number, amplitudes: number[], adsr: number[]) => Promise<any>;
+export declare const setFrequency: (frequency: number) => Promise<any>;
 export declare const stop: () => Promise<any>;
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,10 @@ import ToneGeneratorModule from "./ToneGeneratorModule";
 
 export const getIsPlaying = () => ToneGeneratorModule.getIsPlaying();
 export const playWhiteNoise = () => ToneGeneratorModule.playWhiteNoise();
-export const playPinkNoise = () => ToneGeneratorModule.playPinkNoise();
-export const playBrownNoise = () => ToneGeneratorModule.playBrownNoise();
-export const setWhiteNoiseAmplitude = async (amplitude: number) => ToneGeneratorModule.setWhiteNoiseAmplitude(amplitude);
+export const adjustFrequency = (frequency: number) => ToneGeneratorModule.adjustFrequency(frequency);
 export const playSoundFromURL = (URL: string) => ToneGeneratorModule.playSoundFromURL(URL);
 export const stopSoundFromURL = (URL: string) => ToneGeneratorModule.stopSoundFromURL(URL);
 export const setVolumeForURL = (URL: string, amplitude: number) => ToneGeneratorModule.setVolumeForURL(URL, amplitude)
-export const pinkNoiseTest = (coefficient1: number, coefficient2: number, coefficient3: number,
-	coefficient4: number, coefficient5: number, coefficient6: number, pitch: number) => ToneGeneratorModule.pinkNoiseTest(coefficient1,
-		coefficient2, coefficient3, coefficient4, coefficient5, coefficient6, pitch);
-export const changePitch = (pitch: number) => ToneGeneratorModule.changePitch(pitch);
-export const play = async (
-	frequency: number,
-	amplitudes: number[],
-	adsr: number[],
-) => ToneGeneratorModule.play(frequency, amplitudes, adsr);
 export const performFFT = () => ToneGeneratorModule.performFFT();
-
-export const setFrequency = async (
-	frequency: number,
-	amplitudes: number[],
-	adsr: number[],
-) => ToneGeneratorModule.setFrequency(frequency, amplitudes, adsr);
-
+export const setFrequency = async (frequency: number) => ToneGeneratorModule.setFrequency(frequency);
 export const stop = async () => ToneGeneratorModule.stop();
